f8685b00f371fc3bff33ffe772b2eebe1dedc6f5
Jan <hubeja@delta-studenti.cz>
2021-10-19 19:15:04 +0200

    minor changes

--- routes/web.php
+++ routes/web.php
@@ -55,8 +55,3 @@ Route::get('/', function () {
 Route::get('{any}', function () {
     return view('app');
 })->where('any', '.*');
-
-//???
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
