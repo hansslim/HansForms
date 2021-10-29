594a580046701639f9bb5d65f21d739fbc47647a
Jan <hubeja@delta-studenti.cz>
2021-10-29 13:02:39 +0200

    added package Formulate

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueFormulate from '@braid/vue-formulate'
+
+Vue.use(VueFormulate);
 
 Vue.config.productionTip = false;
 
