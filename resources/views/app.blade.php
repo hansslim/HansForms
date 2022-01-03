15aba6ad0fc635f9dccb43c8cc8bbf73c9966cc0
Jan <hubeja@delta-studenti.cz>
2022-01-03 12:36:16 +0100

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>HansForm</title>
