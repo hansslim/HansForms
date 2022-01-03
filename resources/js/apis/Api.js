b36cc337b867ba8ebb46e656b3b154da658625ac
Jan <hubeja@delta-studenti.cz>
2022-01-03 19:43:41 +0100

    fe api base url moved to env file

--- resources/js/apis/Api.js
+++ resources/js/apis/Api.js
@@ -3,7 +3,7 @@ import router from '../router';
 import store from '../store';
 
 const Api = axios.create({
-    baseURL: 'http://localhost:8000/api'
+    baseURL: process.env.MIX_API_BASE_URL
 });
 
 Api.interceptors.response.use(
