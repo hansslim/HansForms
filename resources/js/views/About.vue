e7d6256d913631e7b078012b0ff234d032301ccf
Jan <hubeja@delta-studenti.cz>
2022-03-05 15:29:53 +0100

    about page

--- resources/js/views/About.vue
+++ resources/js/views/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <h1>HansForms</h1>
+        <p>
+            The main goal of this project is to create a survey web application
+            for processing the survey, where users can fill in the forms, but
+            also manage them. Except for basic functionality which is form
+            rendering followed by the ability to answer the form, the
+            application will contain for example access restrictions depending
+            on the start and end date, private access for invited users, or data
+            export for correlation analysis purposes.
+        </p>
+    </div>
 </template>
